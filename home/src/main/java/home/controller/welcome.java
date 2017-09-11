5a3cb9ea86a1a65cc19330b3ef9234752242b80e
xiaowu <2457893105@qq.com>
2017-09-12 01:05:25 +0800

    springboot

---
+++ home/src/main/java/home/controller/welcome.java
@@ -0,0 +1,12 @@
+package home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@	Controller
+public class welcome {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
---
+++ home/src/main/resources/static/business/login/login.js
@@ -0,0 +1,10 @@
+/**
+ * 登入业务
+ */
+$(document).ready(function(){
+	login();
+});
+
+function login(){
+//	xw_tools.sendAjax("#","123");
+}
